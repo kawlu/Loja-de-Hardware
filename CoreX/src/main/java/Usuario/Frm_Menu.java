7e2e62f168dfecd2159c6ce45020df0bda7ecf79
Yasmin Cibreiros Chagas <114839561+yasminccs@users.noreply.github.com>
2024-10-24 18:29:17 -0300

    add files de usuario v3 (?)

---
+++ CoreX/src/main/java/Usuario/Frm_Menu.java
@@ -0,0 +1,198 @@
+package Usuario;
+
+import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+
+import Usuario.Usuario;
+import Usuario.Usuario.Cliente;
+import Usuario.Frm_Login;
+import Usuario.Usuario.Cliente;
+import Usuario.Frm_Cadastrar;
+
+public class Frm_Menu extends javax.swing.JFrame {
+
+    public Frm_Menu() {
+        initComponents();
+        setLocationRelativeTo(null);
+        //setExtendedState(MAXIMIZED_BOTH);
+    }
+    
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jMenu1 = new javax.swing.JMenu();
+        jMenu2 = new javax.swing.JMenu();
+        jPanel1 = new javax.swing.JPanel();
+        btnLoginUsuarioOP = new javax.swing.JButton();
+        btnCadastroUsuarioOP = new javax.swing.JButton();
+        btnVoltarUsuarioOP = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
+        jMenuBar2 = new javax.swing.JMenuBar();
+        jMenu5 = new javax.swing.JMenu();
+        jMenu6 = new javax.swing.JMenu();
+
+        jMenu1.setText("jMenu1");
+
+        jMenu2.setText("jMenu2");
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jPanel1.setBackground(new java.awt.Color(255, 255, 255));
+
+        btnLoginUsuarioOP.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
+        btnLoginUsuarioOP.setText("Login");
+        btnLoginUsuarioOP.setName("btnLoginUsuarioOP"); // NOI18N
+        btnLoginUsuarioOP.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnLoginUsuarioOPActionPerformed(evt);
+            }
+        });
+
+        btnCadastroUsuarioOP.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
+        btnCadastroUsuarioOP.setText("Cadastro");
+        btnCadastroUsuarioOP.setName("btnCadastroUsuarioOP"); // NOI18N
+        btnCadastroUsuarioOP.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnCadastroUsuarioOPActionPerformed(evt);
+            }
+        });
+
+        btnVoltarUsuarioOP.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
+        btnVoltarUsuarioOP.setText("Voltar");
+        btnVoltarUsuarioOP.setName("btnVoltarUsuarioOP"); // NOI18N
+        btnVoltarUsuarioOP.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnVoltarUsuarioOPActionPerformed(evt);
+            }
+        });
+
+        jLabel1.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+        jLabel1.setText("Escolha uma opção");
+        jLabel1.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
+        jLabel1.setName("lblPerguntaUsuarioOP"); // NOI18N
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(57, 57, 57)
+                        .addComponent(btnLoginUsuarioOP)
+                        .addGap(18, 18, 18)
+                        .addComponent(btnCadastroUsuarioOP)
+                        .addGap(18, 18, 18)
+                        .addComponent(btnVoltarUsuarioOP))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(106, 106, 106)
+                        .addComponent(jLabel1)))
+                .addContainerGap(85, Short.MAX_VALUE))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap(75, Short.MAX_VALUE)
+                .addComponent(jLabel1)
+                .addGap(18, 18, 18)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btnCadastroUsuarioOP)
+                    .addComponent(btnVoltarUsuarioOP)
+                    .addComponent(btnLoginUsuarioOP))
+                .addGap(136, 136, 136))
+        );
+
+        jMenu5.setText("File");
+        jMenuBar2.add(jMenu5);
+
+        jMenu6.setText("Edit");
+        jMenuBar2.add(jMenu6);
+
+        setJMenuBar(jMenuBar2);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void btnLoginUsuarioOPActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnLoginUsuarioOPActionPerformed
+        Frm_Login login = new Frm_Login();
+        login.setVisible(true);
+    }//GEN-LAST:event_btnLoginUsuarioOPActionPerformed
+
+    private void btnCadastroUsuarioOPActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCadastroUsuarioOPActionPerformed
+        Frm_Cadastrar cadastro = new Frm_Cadastrar();
+        cadastro.setVisible(true);
+    }//GEN-LAST:event_btnCadastroUsuarioOPActionPerformed
+
+    private void btnVoltarUsuarioOPActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnVoltarUsuarioOPActionPerformed
+        dispose();
+    }//GEN-LAST:event_btnVoltarUsuarioOPActionPerformed
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(Frm_Menu.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(Frm_Menu.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(Frm_Menu.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(Frm_Menu.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(() -> {
+            new Frm_Menu().setVisible(true);
+            Usuario.main(args);
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnCadastroUsuarioOP;
+    private javax.swing.JButton btnLoginUsuarioOP;
+    private javax.swing.JButton btnVoltarUsuarioOP;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JMenu jMenu1;
+    private javax.swing.JMenu jMenu2;
+    private javax.swing.JMenu jMenu5;
+    private javax.swing.JMenu jMenu6;
+    private javax.swing.JMenuBar jMenuBar2;
+    private javax.swing.JPanel jPanel1;
+    // End of variables declaration//GEN-END:variables
+
+}
